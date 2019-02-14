a4984c5fb98759e7004340ba1d386f302f423af8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-02-14 11:33:42 +0100

    Ignore order of fields in snapshot
    * remove accidentally added license headers
    
    https://github.com/json-snapshot/json-snapshot.github.io/issues/9

--- src/main/java/io/github/jsonSnapshot/matchrule/JSONAssertMatchRule.java
+++ src/main/java/io/github/jsonSnapshot/matchrule/JSONAssertMatchRule.java
@@ -1,34 +1,11 @@
 package io.github.jsonSnapshot.matchrule;
 
-import lombok.NonNull;
-
 import org.json.JSONException;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import io.github.jsonSnapshot.SnapshotMatchException;
-
-/*
- * #%L
- * json-snapshot.github.io
- * %%
- * Copyright (C) 2019 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+import lombok.NonNull;
 
 public class JSONAssertMatchRule implements SnapshotMatchRule {
   public static final JSONAssertMatchRule INSTANCE_LENIENT =
